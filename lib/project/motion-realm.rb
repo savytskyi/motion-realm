a256599af7d5e11e50c1a31509bd83cad2c65477
Kyrylo Savytskyi <mail@savytskyi.com>
2016-11-22 14:07:34 +0200

    set default values with hash

--- lib/project/motion-realm.rb
+++ lib/project/motion-realm.rb
@@ -8,13 +8,11 @@ module MotionRealm
       # # Does not work correct in current version of RM or Realm:
       # self.alloc.initWithValue opts
 
-      # object = self.new
-      object = self.alloc.init
+      object = self.new
       opts.each do |key, value|
         method_name = "set" + key.to_s.capitalize
         method_name = key.to_s + "="
 
-        # object.performSelector method_name, withObject: value
         object.send(method_name, value)
       end
 
@@ -34,33 +32,30 @@ module MotionRealm
       all.last
     end
 
-    # def defaultPropertyValues
-    #   # set all properties to nil
-    #   values = {}
+    def defaultPropertyValues
+      # set all properties to nil
+      values = {}
 
-    #   self.schema.properties_by_name.each do |name, property|
-    #     mp "Name: #{name}/#{name.name}/#{name.type}; property: #{property}"
-    #     values[name] = if default_values[name]
-    #       default_values[name]
-    #     elsif property.nil?
-    #       nil
-    #     elsif property.bool?
-    #       false
-    #     elsif property.number? || property.mixed?
-    #       0
-    #     elsif property.array?
-    #       []
-    #     else
-    #       nil
-    #     end
-    #   end
+      self.schema.properties_by_name.each do |name, property|
+        values[name] = if default_values[name]
+          default_values[name]
+        elsif property.bool?
+          false
+        elsif property.number? || property.mixed?
+          0
+        elsif property.array?
+          []
+        else
+          nil
+        end
+      end
 
-    #   values
-    # end
+      values
+    end
 
-    # def default_values
-    #   {}
-    # end
+    def default_values
+      {}
+    end
 
     def indexedProperties
       indexes
